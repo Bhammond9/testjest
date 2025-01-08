44cf50272a85f064d254797d72d4f8f5152b83e8
Bhammond9 <142949554+Bhammond901@users.noreply.github.com>
2025-01-08 16:32:12 -0700

    multiple test structure

--- test/Utils.test.ts
+++ test/Utils.test.ts
@@ -1,8 +1,33 @@
-import { toUpperCase } from "../src/app/Utils";
+import { getStringInfo, toUpperCase } from "../src/app/Utils";
 
 describe('Utils test suite', ()=>{
-    test('should reuturn uppercase', ()=>{
-        const result = toUpperCase('abc');
-        expect(result).toBe('ABC');
+    it('should reuturn uppercase of a valid string', ()=>{
+
+        //arrange:
+        const sut = toUpperCase;
+        const expected = 'ABC'
+
+        // act:
+        const actual = sut('abc');
+
+        // assert:
+        expect (actual).toBe(expected)
     })
+
+    it.only('should return info for valid string', ()=>{
+        const actual = getStringInfo('My-String');
+        expect(actual.lowerCase).toBe('my-string');
+        expect(actual.extraInfo).toEqual({})
+        expect(actual.characters.length).toBe(9);
+        expect(actual.characters).toHaveLength(9);
+        expect(actual.characters).toEqual(['M', 'y', '-','S', 't', 'r','i', 'n', 'g']);
+        expect(actual.characters).toContain<string>('M');
+        expect(actual.characters).toEqual(
+            expect.arrayContaining(['S', 't', 'r','i', 'n', 'g', 'M', 'y', '-'])
+        );
+        expect(actual.extraInfo).not.toBe(undefined);
+        expect(actual.extraInfo).not.toBeUndefined();
+        expect(actual.extraInfo).toBeDefined();
+        expect(actual.extraInfo).toBeTruthy();
+    });
 });
